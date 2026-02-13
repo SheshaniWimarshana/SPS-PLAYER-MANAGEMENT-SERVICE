440a7d6b95c42cc8a741e69dcd6a8f243fc4ea1a
SheshaniWimarshana <136824090+SheshaniWimarshana@users.noreply.github.com>
2026-02-13 12:52:27 +0530

    Create DuplicatePlayerException class
    Added a custom exception for handling duplicate player creation.

--- player-service/src/main/java/com/sps/player/exception/DuplicatePlayerException.java
+++ player-service/src/main/java/com/sps/player/exception/DuplicatePlayerException.java
@@ -1,1 +1,18 @@
+package com.sps.player.exception;
 
+/**
+ * Exception thrown when attempting to create a duplicate player
+ *
+ * @author SPS Cricket Club
+ * @version 1.0.0
+ */
+public class DuplicatePlayerException extends RuntimeException {
+
+    public DuplicatePlayerException(String message) {
+        super(message);
+    }
+
+    public DuplicatePlayerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
