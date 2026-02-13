b0251889becc1ff2ce1b6832884786f90c29fcc0
SheshaniWimarshana <136824090+SheshaniWimarshana@users.noreply.github.com>
2026-02-13 12:51:22 +0530

    Create PlayerRequestDTO with validation annotations
    Added validation annotations for player name and birthday fields.

--- player-service/src/main/java/com/sps/player/dto/PlayerRequestDto.java
+++ player-service/src/main/java/com/sps/player/dto/PlayerRequestDto.java
@@ -1,1 +1,33 @@
+package com.sps.player.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * Request DTO for creating/updating Player
+ *
+ * @author SPS Cricket Club
+ * @version 1.0.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerRequestDTO {
+
+    @NotBlank(message = "Player name is required and cannot be blank")
+    private String name;
+
+    @NotNull(message = "Birthday is required")
+    @Past(message = "Birthday must be in the past")
+    private LocalDate birthday;
+
+    private String imageName;
+
+    private String status = "ACTIVE";
+}
