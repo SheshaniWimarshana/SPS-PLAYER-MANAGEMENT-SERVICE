c6990af064cb4044d9f8a04a98f9e350c8991dc0
SheshaniWimarshana <136824090+SheshaniWimarshana@users.noreply.github.com>
2026-02-13 12:54:19 +0530

    Add PlayerNotFoundException for player lookup errors

--- player-service/src/main/java/com/sps/player/exception/PlayerNotFoundException.java
+++ player-service/src/main/java/com/sps/player/exception/PlayerNotFoundException.java
@@ -1,1 +1,18 @@
+package com.sps.player.exception;
 
+/**
+ * Exception thrown when a player is not found
+ *
+ * @author SPS Cricket Club
+ * @version 1.0.0
+ */
+public class PlayerNotFoundException extends RuntimeException {
+
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+
+    public PlayerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
