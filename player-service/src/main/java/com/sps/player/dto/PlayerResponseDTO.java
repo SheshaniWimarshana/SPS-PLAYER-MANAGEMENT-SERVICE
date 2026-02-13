9e73c5e72675ac803e2713b9d20a5734d8cb7025
SheshaniWimarshana <136824090+SheshaniWimarshana@users.noreply.github.com>
2026-02-13 12:51:43 +0530

    Create PlayerResponseDTO for player data response
    Added PlayerResponseDTO class for API responses.

--- player-service/src/main/java/com/sps/player/dto/PlayerResponseDTO.java
+++ player-service/src/main/java/com/sps/player/dto/PlayerResponseDTO.java
@@ -1,1 +1,30 @@
+package com.sps.player.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Response DTO for Player
+ * Used for API responses
+ *
+ * @author SPS Cricket Club
+ * @version 1.0.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerResponseDTO {
+
+    private Long id;
+    private String name;
+    private LocalDate birthday;
+    private String imageName;
+    private String status;
+    private Integer age;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
